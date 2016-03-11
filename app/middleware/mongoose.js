1ab266582df1e9828bf055f8a59db06322443fea
Antony Donovan <antony.donovan@generalassemb.ly>
2016-03-10 20:50:00 -0500

    Add export of mongoose middleware.
    Previous omission.

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -4,3 +4,5 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/express-template';
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
+
+module.exports = mongoose;

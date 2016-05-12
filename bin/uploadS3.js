4ad17da870d9cae9cca24ccd5a4ce859f618b29e
susandaiute <susandaiute@gmail.com>
2016-05-12 15:29:18 -0400

    Move S3 specifics to separate file
    Refactor AWS only code  in lib/awsfile.js
    Leave cli upload code in bin/uploadS3.js

--- bin/uploadS3.js
+++ bin/uploadS3.js
@@ -3,12 +3,10 @@
 require('dotenv').config();
 
 const fs = require('fs');
-const crypto = require('crypto');
 
 const fileType = require('file-type');
-const AWS = require('aws-sdk');
 
-const s3 = new AWS.S3();
+const awsS3Upload = require('../lib/awsfile');
 
 // always return object with extension and mime.
 // fileType returns null when no match
@@ -31,50 +29,13 @@ const readFile = (filename) =>
     });
   });
 
-const randomHexString = (length) =>
-  new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (err, buf) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(buf.toString('hex'));
-      }
-    });
-  });
-
-
-const awsUpload = (file) =>
-  randomHexString(16)
-  .then((filename) => {
-    let dir = new Date().toISOString().split('T')[0];
-    return {
-      ACL: 'public-read',
-      Body: file.data,
-      Bucket: 'susandaiute',
-      ContentType: file.mime,
-      Key: `${dir}/${filename}.${file.ext}`,
-    };
-  }).then(params =>
-    new Promise((resolve, reject) => {
-      s3.upload(params, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    })
-  );
-
 readFile(filename)
   .then((data) => {
     let file = mimeType(data);
     file.data = data;
     return file;
   })
-  .then(awsUpload)
+  .then(awsS3Upload)
   .then((s3response) =>
     console.log(s3response))
   .catch(console.error);
-
-  

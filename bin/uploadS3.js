83c2bbc48d9c3d4dfc071cb16a6286410fa41ad1
susandaiute <susandaiute@gmail.com>
2016-05-12 13:27:14 -0400

    Read file/get type
    User file-type to determine content type for S3 upload

--- bin/uploadS3.js
+++ bin/uploadS3.js
@@ -1,29 +1,45 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
+
+const mimeType = (data) =>
+  Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream',
+  }, fileType(data));
 
 let filename = process.argv[2] || '';
 
 const readFile = (filename) =>
-  new Promise ((resolve, reject) => {
+  new Promise((resolve, reject) => {
     fs.readFile(filename, (err, data) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(data);
-    }
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
   });
-});
 
-const awsS3UploadOptions = {
-  ACL: 'public-read',
-  Body: '',
-  Bucket: 'susandaiute',
-  ContentType: 'application/octet-stream',
-  Key: 'test/test.bin'
+const awsUpload = (file) => {
+  const options = {
+    ACL: 'public-read',
+    Body: file.data,
+    Bucket: 'gaand-011',
+    ContentType: file.mime,
+    Key: `test/test.${file.ext}`,
+  };
+  return Promise.resolve(options);
 };
 
 readFile(filename)
-.then((data) =>
-console.log(`${filename} is ${data.length} bytes long`))
+.then((data) => {
+  let file = mimeType(data);
+  file.data = data;
+  return file;
+})
+.then(awsUpload)
+.then((options) =>
+  console.log(options))
 .catch(console.error);
